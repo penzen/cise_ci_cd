9e83948244b21149ebbde95cd4bec87ed0ebb278
penzen <59159051+penzen@users.noreply.github.com>
2021-04-06 15:34:09 +1200

    Syntax error, 4 was in the wrong place

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ import React from 'react';
  ReactDOM.unmountComponentAtNode(div);
  });
  describe('Addition', () => {
-  it('knows that 2 and 2 make 4', (4) => {
+  it('knows that 2 and 2 make 4', () => {
   expect(2 + 2).toBe(4);
   });
   });
